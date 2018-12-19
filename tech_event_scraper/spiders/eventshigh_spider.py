20bb0b16fdf81e40b938c48fb4412eb606c2ad58
Ramaswamy Venkatachalam <ramasamynasa@gmail.com>
2018-12-19 16:01:01 +0530

    change to common data schema

--- tech_event_scraper/spiders/eventshigh_spider.py
+++ tech_event_scraper/spiders/eventshigh_spider.py
@@ -2,6 +2,7 @@ import scrapy
 import re
 from scrapy import signals
 import json
+from datetime import datetime
 
 event_data = {}
 
@@ -25,26 +26,40 @@ class EventshighSpider(scrapy.Spider):
     def parse_event_data(self, response):
         for item in response.xpath("//div[@class='m-sm-lr-16 browse-events-wrp']//div[@class='d-inline-block event-card-wrp valign-top ga-card-track browse-card']"):
             event_title = item.xpath("a//div[@class='truncate f-s-16 f-s-sm-12 l-h-1p5 color-dark-grey']//text()").extract_first()
-            event_date = item.xpath("a//div[@class='truncate f-s-16 f-s-sm-12 f-w-500 l-h-1p5 color-dark-grey']//text()").extract_first().strip()
+            date_string = item.xpath("a//div[@class='truncate f-s-16 f-s-sm-12 f-w-500 l-h-1p5 color-dark-grey']//text()").extract_first().strip()
             event_venue = item.xpath("a//div[@class='truncate f-s-16 f-s-sm-12 l-h-1p5 color-light-grey text-capitalize']//text()").extract_first()
-            event_description = item.xpath("a//div[@class='truncate f-s-16 f-s-sm-12 f-w-500 l-h-1p5 color-dark-grey'][2]/text()").extract_first()
+            event_fee = item.xpath("a//div[@class='truncate f-s-16 f-s-sm-12 f-w-500 l-h-1p5 color-dark-grey'][2]/text()").extract_first()
             event_city = re.findall(".com/(.*)/technology", response.url)[0]
+            event_keywords = ", ".join(item.xpath("div[@class='truncate']//a[@class='category-pill d-inline-block f-s-12 f-s-sm-8 f-w-sm-500 text-capitalize']//text()").extract())
             event_url = "https://www.eventshigh.com" + item.xpath("a/@href").extract_first()
+            date_st = re.findall("[A-Z]{1}[a-z]{2}, ([0-9]{1,2} [A-Z]{1}[a-z]{2} [0-9]{0,4}) ?, ([0-9]{1,2}[:][0-9]{1,2}[A-Z]+)", date_string)
+            try:
+                event_date = datetime.strptime(date_st[0][0], "%d %b ").strftime(f"{datetime.now().year}-%m-%d")
+            except ValueError:
+                event_date = datetime.strptime(date_st[0][0], "%d %b %Y").strftime("%Y-%m-%d")
+            except:
+                event_date = date_string
+            try:
+                event_time = date_string[0][1]
+            except IndexError:
+                event_time = None
+            except:
+                event_time = None
             event_data['Eventshigh Events'].append(
                 {
                     'event_title': event_title,
-                    'event_date': event_date,
                     'event_venue': event_venue,
+                    'event_description': None,
+                    'event_url': event_url,
+                    'event_date': event_date,
+                    'event_keywords': event_keywords,
+                    'event_fee': event_fee,
                     'event_city': event_city,
-                    'event_fee': event_description,
-                    'event_url': event_url
+                    'event_time': event_time,
                 })
-            print(event_data)
 
     def spider_closed(self):
         with open('../tech events/EventshighEvents.json', 'w', encoding='utf-8') as file:
             json.dump(event_data, file, ensure_ascii=False)
-
-
 
 
